31e48bb33e0b30ebf96760d4e883c4b9c4c21d2b
Robert Kuehfus <rkuehfus@live.com>
2019-01-07 13:42:52 -0500

    fixed deployment script

--- Student/Resources/DeployMonWorkshopEnv.ps1
+++ Student/Resources/DeployMonWorkshopEnv.ps1
@@ -19,7 +19,7 @@ $MonWorkShopVaultName = $monWorkShopName + 'MonWorkshopVault'
 New-AzKeyVault -VaultName $MonWorkShopVaultName -ResourceGroupName $rg.ResourceGroupName -Location $rg.Location -EnabledForTemplateDeployment
 
 #Step 4: Add password as a secret.  Note:this will prompt you for a user and password.  User should be vmadmin and a password that meet the azure pwd police like P@ssw0rd123!!
-Set-AzureKeyVaultSecret -VaultName $MonWorkShopVaultName -Name "VMPassword" -SecretValue (Get-Credential).Password
+Set-AzKeyVaultSecret -VaultName $MonWorkShopVaultName -Name "VMPassword" -SecretValue (Get-Credential).Password
 
 #Step 5: Update azuredeploy.parameters.json file with your envPrefixName and Key Vault info example- /subscriptions/{guid}/resourceGroups/{group-name}/providers/Microsoft.KeyVault/vaults/{vault-name}
 (Get-AzKeyVault -VaultName $MonWorkShopVaultName).ResourceId
